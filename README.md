f662e326a7781455b02cab9a965123e1423fde71
KikuBruce <kikubruce@qq.com>
2020-01-04 14:18:38 +0800

    add config.yml/config.py

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Kiku_Test_Framework
+
+## 环境
+
+Python3
+
+## 目录结构
+Kiku_Test_Framework
+	|--config
+	|--data
+	|--drivers
+	|--log
+	|--report
+	|--test_case
+	|--common
+
---
+++ common/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-01-04 14:15:21
+# @Author  : linlin (kikubruce@qq.com)
+# @Link    : ${link}
+# @Version : $Id$
+
+"""
+读取config配置
+
+"""
