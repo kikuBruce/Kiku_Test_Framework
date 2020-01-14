4c69993bcbaf19cba5fb14926c44b326c8a81272
KikuBruce <kikubruce@qq.com>
2020-01-14 23:15:36 +0800

    修改了demo为从openAPI接口获取城市天气；增加了ExcelReader方法

--- common/file_reader.py
+++ common/file_reader.py
@@ -4,6 +4,7 @@
 # @Author  : Lin lin
 
 import yaml
+from xlrd import open_workbook
 import os
 
 
@@ -26,3 +27,55 @@ class YamlReader:
             with open(self.yaml_f, 'r') as f:
                 self._data = list(list(yaml.safe_load_all(f)))
             return self._data
+
+
+class ExcelReader:
+    """用于读取Excel,提供返回带title的键值对列表和不含title的列表
+
+    """
+
+    def __init__(self, excel_file, sheet=0, title=True):
+        if os.path.exists(excel_file):
+            self.excel_f = excel_file
+        else:
+            raise FileNotFoundError("excel文件不存在")
+
+        self.sheet = sheet
+        self.title = title
+        self._data = list()
+
+    @property
+    def data(self):
+        # 首次调用
+        if not self._data:
+            workbook = open_workbook(self.excel_f)
+            # 根据index类型返回操作句柄
+            if isinstance(self.sheet, int):
+                sheet = workbook.sheet_by_index(sheetx=self.sheet)
+            elif isinstance(self.sheet, str):
+                sheet = workbook.sheet_by_name(sheet_name=self.sheet)
+            else:
+                raise TypeError("sheet 参数应该是int或者str类型，而不是{}".format(type(self.sheet)))
+
+            # 根据tittle的布尔值返回
+            # 如果为真，把可迭代的titles和values组合为键值对，每一行作为字典追加到列表，否则直接追加到列表
+            if self.title:
+                titles = sheet.row_values(rowx=0)
+                for col in range(1, sheet.nrows):
+                    values = sheet.row_values(rowx=col)
+                    _dict = dict(zip(titles, values))
+                    self._data.append(_dict)
+            else:
+                for col in range(1, sheet.nrows):
+                    values = sheet.row_values(rowx=col)
+                    self._data.append(values)
+
+        return self._data
+
+
+if __name__ == '__main__':
+    from common.config import DATA_PATH
+
+    excel_file = os.path.join(DATA_PATH, 'CityCode.XLS')
+    excel_reader = ExcelReader(excel_file=excel_file, sheet=0, title=True)
+    print(excel_reader.data)
--- test_case/test_get_weather.py
+++ test_case/test_get_weather.py
@@ -6,14 +6,20 @@
 
 import requests
 import unittest
-from common.config import Config
+import os
+import json
+from common.config import Config, DATA_PATH
 from common.log import log
+from common.file_reader import ExcelReader
 
 log = log()
 
 
 class TestBaidu(unittest.TestCase):
     URL = Config().get_config(element='url', index=0)
+
+    excel_file = os.path.join(DATA_PATH, 'CityCode.XLS')
+    excel_data = ExcelReader(excel_file=excel_file, title=True, sheet=0).data
 
     def setUp(self) -> None:
         pass
@@ -22,12 +28,27 @@ class TestBaidu(unittest.TestCase):
         pass
 
     def test_get_baidu(self):
-        rsp_code = requests.get(self.URL)
-        if rsp_code.status_code == 200:
-            log.info('Case Pass')
-        else:
-            log.error('response code should be 200 ,not be:{}'.format(rsp_code.status_code))
+        city_name = "深圳"
+        log.info('*' * 20 + "获取城市编码" + '*' * 20)
+        for index in range(0, len(self.excel_data)):
+            if self.excel_data[index].get('ChinsesName') == city_name:
+                city_code = self.excel_data[index].get('CityCode')
+                log.info("获取城市编码成功，城市名称：{0}； CityCode: {1}".format(city_name, city_code))
+                break
 
+        _url = Config().get_config(element='url', index=0) + '?' + 'id={}'.format(city_code)
+        rsp = requests.get(url=_url)
+        self.assertEqual(rsp.status_code, 200)
+        dict_rsp = json.loads(rsp.text)
+        log.info("{0} {1} \
+        \t 温度: {2} \
+        \t 天气：{3} \
+        \t pm2.5：{4}".format(city_name, dict_rsp.get('today'),
+                             dict_rsp.get('city'),
+                             dict_rsp.get('temp'),
+                             dict_rsp.get('weather'),
+                             dict_rsp.get('pm25')
+                             ))
 
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
